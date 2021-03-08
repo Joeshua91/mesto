601c5acf0d436ef0e6fd3a2fe8fc50716e464a2b
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-03-08 20:18:44 +0500

    feat: сделать инициализацию карточек и добавление новой карточки в начало

--- script/index.js
+++ script/index.js
@@ -25,56 +25,9 @@ const initialCards = [
   }
 ];
 
-const placeSection = document.querySelector('.places__section');
-const formPopupAdd = document.querySelector('.popup__form_type_add'); // потом удалить
-
-function createPlace(item) {
-  return `
-    <li class="place-card">
-      <img class="place-card__image" src="${item.link}" alt="${item.name}">
-      <div class="place-card__wrapper">
-        <h2 class="place-card__name">${item.name}</h2>
-        <button class="place-card__like" type="button" aria-label="like"></button>
-      </div>
-    </li>
-  `
-};
-
-function renderPlace() {
-  const result = initialCards.map(createPlace).join('');
-
-  placeSection.insertAdjacentHTML('afterbegin', result);
-}
-
-function addPlace(evt) {
-  evt.preventDefault();
-  const inputPlaceTitle = formPopupAdd.querySelector('.popup__input_type_title-place');
-  const placeTitle = inputPlaceTitle.value;
-  const inputPlaceLink = formPopupAdd.querySelector('.popup__input_type_link-place');
-  const placeLink = inputPlaceLink.value;
-
-  const newPlace = createPlace({
-    name: placeTitle,
-    link: placeLink,
-  });
-
-  placeSection.insertAdjacentHTML('afterbegin', newPlace);
-
-  inputPlaceTitle.value = '';
-  inputPlaceLink.value = '';
-};
-
-
-renderPlace();
-formPopupAdd.addEventListener('submit', addPlace);
-
-
-
-
-
-
-
 /* - - - - - - - - - - */
+
+/* = ПЕРЕМЕННЫЕ = */
 
 const userName = document.querySelector('.user__name');
 const userVocation = document.querySelector('.user__vocation');
@@ -87,7 +40,19 @@ const popupAdd = document.querySelector('.popup_type_add');
 const closePopupButton = document.querySelectorAll('.popup__button-close');
 const savePopupButton = document.querySelector('.popup__button-save');
 const formPopupEdit = document.querySelector('.popup__form_type_edit');
-// const formPopupAdd = document.querySelector('.popup__form_type_add');
+const formPopupAdd = document.querySelector('.popup__form_type_add');
+const placeSection = document.querySelector('.places__section');
+const templatePlaceCard = document.querySelector('.place-template');
+const inputPlaceTitle = formPopupAdd.querySelector('.popup__input_type_title-place');
+const inputPlaceLink = formPopupAdd.querySelector('.popup__input_type_link-place');
+
+
+/* = ФУНКЦИИ = */
+
+const initial = () => {
+  const initialResult = initialCards.map(createPlace);
+  placeSection.append(...initialResult);
+};
 
 const openPopup = popup => {
   popup.classList.toggle('popup_opened');
@@ -97,12 +62,48 @@ const closePopup = popup => {
   popup.classList.toggle('popup_opened');
 };
 
-const formSubmitHandler = evt => {
-  evt.preventDefault();
+const formEditSubmitHandler = event => {
+  event.preventDefault();
   userName.textContent = nameInput.value;
   userVocation.textContent = vocationInput.value;
   closePopup(popupEdit);
 };
+
+const formAddSubmitHandler = event => {
+  event.preventDefault();
+  const addPlace = {
+    name: '',
+    link: '',
+  };
+
+  addPlace.name = inputPlaceTitle.value;
+  addPlace.link = inputPlaceLink.value;
+
+  renderPlace(addPlace);
+  closePopup(popupAdd);
+};
+
+const createPlace = item => {
+  const placeCard = templatePlaceCard.content.cloneNode(true);
+  const placeCardImage = placeCard.querySelector('.place-card__image');
+  const placeCardName = placeCard.querySelector('.place-card__name')
+  placeCardImage.src = item.link;
+  placeCardName.textContent = item.name;
+
+  return placeCard;
+};
+
+const renderPlace = item => {
+  placeSection.prepend(createPlace(item));
+};
+
+
+/* = ВЫЗОВЫ ФУНКЦИЙ = */
+
+initial();
+
+
+/* = ОБРАБОТЧИКИ СОБЫТИЙ = */
 
 buttonEdit.addEventListener('click', () => {
   nameInput.value = userName.textContent;
@@ -115,9 +116,10 @@ buttonAdd.addEventListener('click', () => {
 });
 
 closePopupButton.forEach(item => {
-  item.addEventListener('click', (evt) => {
-    closePopup(evt.target.closest('.popup'));
+  item.addEventListener('click', (event) => {
+    closePopup(event.target.closest('.popup'));
   });
 });
 
-formPopupEdit.addEventListener('submit', formSubmitHandler);
+formPopupEdit.addEventListener('submit', formEditSubmitHandler);
+formPopupAdd.addEventListener('submit', formAddSubmitHandler);
