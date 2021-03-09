661c30d4a1ebaa78dc9641b5006858d7a6b89b7d
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-03-09 11:59:43 +0500

    feat: настроить открытие попапа с картинкой из карточки

--- script/index.js
+++ script/index.js
@@ -45,12 +45,14 @@ const placeSection = document.querySelector('.places__section');
 const templatePlaceCard = document.querySelector('.place-template');
 const inputPlaceTitle = formPopupAdd.querySelector('.popup__input_type_title-place');
 const inputPlaceLink = formPopupAdd.querySelector('.popup__input_type_link-place');
+const popupView = document.querySelector('.popup_type_view');
 
 
 /* = ФУНКЦИИ = */
 
 const initialPlaces = () => {
   const initialResult = initialCards.map(createPlace);
+
   placeSection.append(...initialResult);
 };
 
@@ -60,13 +62,16 @@ const togglePopup = popup => {
 
 const formEditSubmitHandler = e => {
   e.preventDefault();
+
   userName.textContent = nameInput.value;
   userVocation.textContent = vocationInput.value;
+
   togglePopup(popupEdit);
 };
 
 const formAddSubmitHandler = e => {
   e.preventDefault();
+
   const addPlace = {
     name: '',
     link: '',
@@ -79,23 +84,27 @@ const formAddSubmitHandler = e => {
   togglePopup(popupAdd);
 };
 
-const createPlace = item => {
+const createPlace = i => {
   const placeCard = templatePlaceCard.content.cloneNode(true);
   const placeCardImage = placeCard.querySelector('.place-card__image');
   const placeCardName = placeCard.querySelector('.place-card__name');
   const placeCardLike = placeCard.querySelector('.place-card__like');
   const placeCardDelete = placeCard.querySelector('.place-card__delete');
-  placeCardImage.src = item.link;
-  placeCardName.textContent = item.name;
+
+  placeCardImage.src = i.link;
+  placeCardName.textContent = i.name;
   placeCardImage.alt = placeCardName.textContent;
   placeCardLike.addEventListener('click', likeHandler);
   placeCardDelete.addEventListener('click', deleteHandler);
+  placeCardImage.addEventListener('click', () => {
+    viewPlaceCard(i);
+  });
 
   return placeCard;
 };
 
-const renderPlace = item => {
-  placeSection.prepend(createPlace(item));
+const renderPlace = i => {
+  placeSection.prepend(createPlace(i));
 };
 
 const likeHandler = (e) => {
@@ -104,6 +113,17 @@ const likeHandler = (e) => {
 
 const deleteHandler = (e) => {
   e.target.parentElement.remove();
+}
+
+const viewPlaceCard = i => {
+  const viewPlaceCardImage = document.querySelector('.popup__image');
+  const viewPlaceCardName = document.querySelector('.popup__figcaption');
+
+  viewPlaceCardImage.src = i.link;
+  viewPlaceCardImage.alt = i.name;
+  viewPlaceCardName.textContent = i.name;
+
+  togglePopup(popupView);
 }
 
 
@@ -117,6 +137,7 @@ initialPlaces();
 buttonEdit.addEventListener('click', () => {
   nameInput.value = userName.textContent;
   vocationInput.value = userVocation.textContent;
+
   togglePopup(popupEdit);
 });
 
@@ -124,8 +145,8 @@ buttonAdd.addEventListener('click', () => {
   togglePopup(popupAdd);
 });
 
-closePopupButton.forEach(item => {
-  item.addEventListener('click', (e) => {
+closePopupButton.forEach(i => {
+  i.addEventListener('click', (e) => {
     togglePopup(e.target.closest('.popup'));
   });
 });
