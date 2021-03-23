8bf2c2031333251661307002887f87439eba3c8b
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-03-23 13:14:22 +0500

    feat: написать логику валидации форм и написать стили для показа ошибок в форме

--- script/validate.js
+++ script/validate.js
@@ -1,107 +1,128 @@
 /* = ПРОВЕРЯЕМ ПУСТЫЕ ЛИ ПОЛЯ ВВОДА = */
+
 const allInputsEmpty = (inputList) => {
   return !inputList.some(inputElement => inputElement.value.length > 0);
 };
 
+
 /* = ФУНКЦИЯ НЕВАЛИДНОГО ПОЛЯ ВВОДА = */
+
 const hasInvalidInput = (inputList) => {
-  return !inputList.some(inputElement => inputElement.validity.valid);
+  return inputList.some(inputElement => !inputElement.validity.valid);
 };
 
+
 /* = ФУНКЦИЯ ПЕРЕКЛЮЧЕНИЯ СОСТОЯНИЯ КНОПКИ = */
-const toogleButtonState = (inputList, buttonElement) => {
+
+const setButtonState = (inputList, buttonElement, invalidButtonClass) => {
   // если хотя бы один input невалидный или если есть пустые поля
   if (hasInvalidInput(inputList) || allInputsEmpty(inputList)) {
-    // делаем кнопку НЕАКТИВНОЙ
-    buttonElement.classList.add('popup__button-save_invalid');
+    // сделать кнопку НЕАКТИВНОЙ
+    buttonElement.classList.add(invalidButtonClass);
     buttonElement.setAttribute('disabled', true);
   }
   else {
-    // иначе делаем кнопку АКТИВНОЙ
-    buttonElement.classList.remove('popup__button-save_invalid');
+    // сделать кнопку АКТИВНОЙ
+    buttonElement.classList.remove(invalidButtonClass);
     buttonElement.removeAttribute('disabled');
   }
 };
 
-/* = ФУНКЦИЯ ПОКАЗА ОШИБКИ = */
-const showInputError = (formElement, inputElement) => {
+
+/* = ФУНКЦИЯ ПОКАЗА СООБЩЕНИЯ ОБ ОШИБКE = */
+
+const showErrorMessage = (formElement, inputElement, validationMessage,
+  inputErrorClass, errorClass) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  // показываем ошибку
-  inputElement.classList.add('popup__input_type_error');
-  errorElement.textContent = inputElement.validationMessage;
-  errorElement.classList.add('popup__input-error_active');
+  // ПОКАЗАТЬ сообщение об ошибке
+  inputElement.classList.add(inputErrorClass);
+  errorElement.classList.add(errorClass);
+  errorElement.textContent = validationMessage;
 };
 
-/* = УБИРАЕМ ПОКАЗ ОБ ОШИБКE = */
-const hideInputError = (formElement, inputElement) => {
+/* = ФУНКЦИЯ СКРЫТИЯ СООБЩЕНИЯ ОБ ОШИБКE = */
+
+const hideErrorMessage = (formElement, inputElement,
+  inputErrorClass, errorClass) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  // убираем показ ошибки
-  inputElement.classList.remove('popup__input_type_error');
+  // УБРАТЬ сообщение об ошибке
+  inputElement.classList.remove(inputErrorClass);
+  errorElement.classList.remove(errorClass);
   errorElement.textContent = '';
-  errorElement.classList.remove('popup__input-error_active');
 };
+
 
 /* = ФУНКЦИЯ ПРОВЕРКИ ПОЛЯ ВВОДА НА ВАЛИДНОСТЬ = */
-const checkInput = (formElement, inputElement) => {
+
+const checkInputValid = (validSelector, formElement, inputElement) => {
   // если поле ввода ВАЛИДНО
   if (inputElement.validity.valid) {
-    // УБИРАЕМ сообщение об ошибке
-    hideInputError(formElement, inputElement);
+    // СКРЫТЬ сообщение об ошибке
+    hideErrorMessage(formElement, inputElement,
+      validSelector.inputErrorClass, validSelector.errorClass);
   }
   // если поле ввода НЕВАЛИДНО
   else {
-    // ПОКАЗЫВАЕМ сообщение об ошибке
-    showInputError(formElement, inputElement);
+    // ПОКАЗАТЬ сообщение об ошибке
+    showErrorMessage(formElement, inputElement, inputElement.validationMessage,
+      validSelector.inputErrorClass, validSelector.errorClass);
   }
 };
 
-/* = НАВЕШИВАЕМ ОБРАБОТЧИКИ ПОЛЯМ ВВОДА = */
-const setInputListeners = (formElement) => {
-  // массив полей ввода
-  const inputList = Array.from(
-    formElement.querySelectorAll('.popup__input')
-  );
 
-  // кнопка submit
-  const buttonElement = formElement.querySelector('.popup__button-save');
+/* = НАВЕШАТЬ ОБРАБОТЧИКИ ПОЛЯМ ВВОДА = */
 
-  inputList.forEach(inputElement => {
+const setInputListeners = (validSelector, formElement) => {
+  // создать массив полей ввода
+  const inputList = Array.from(formElement.querySelectorAll(validSelector.inputSelector));
+  // найти кнопки submit
+  const buttonElement = formElement.querySelector(validSelector.buttonSelector);
+
+  // обработать отдельно взятое поле ввода
+  inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
-      // проверяем состояние поля на валидность
-      checkInput(formElement, inputElement);
-      // переключаем состояние кнопки submit
-      toogleButtonState(inputList, buttonElement);
-    });
+      // проверить состояние поле ввода на валидность
+      checkInputValid(validSelector, formElement, inputElement);
 
-    toogleButtonState(inputList, buttonElement);
+      // переключить состояние кнопки submit
+      setButtonState(inputList, buttonElement, validSelector.invalidButtonClass);
+    });
+    // проверить состояние полей
+    // и сделать состояние кнопки submit актуальной при загрузке страницы
+    setButtonState(inputList, buttonElement, validSelector.invalidButtonClass); // ???
+    // почему не работает???
+    // почему консоль возвращает значение 'undefined' здесь,
+    // а в предыдущем вызове функции всё ok?
+    // console.log(setButtonState(inputList, buttonElement, validSelector.invalidButtonClass));
   });
 };
 
-/* = ФУНКЦИЯ ВАЛИДАЦИИ ФОРМ = */
-const enableValidation = () => {
-  // массив всех форм
-  const formList = Array.from(document.querySelectorAll('.popup__form'));
 
-  formList.forEach(formElement => {
+/* = ФУНКЦИЯ ВАЛИДАЦИИ ФОРМ = */
+
+const enableValidation = (validSelector) => {
+  // создать массив всех форм
+  const formList = Array.from(document.querySelectorAll(validSelector.formSelector));
+
+  // обработать отдельно взятую форму
+  formList.forEach((formElement) => {
+    // отменить дефолтное поведение при 'submit'
     formElement.addEventListener('submit', (evt) => {
       evt.preventDefault();
     });
-
-    // слушатель для полей формы
-    setInputListeners(formElement);
-  }
-  )
+    // навесить слушатели для полей формы
+    setInputListeners(validSelector, formElement);
+  });
 };
 
-enableValidation(
-  /*
-  {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  buttonSelector: '.popup__button-save',
-  invalidButtonClass: 'popup__button-save_invalid'
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__input-error_active',
-}
-*/
-);
+
+/* = ВЫЗОВ ФУНКЦИИ ВАЛИДАЦИИ ФОРМ = */
+
+enableValidation({
+  formSelector: '.popup__form', // 'селектор формы
+  inputSelector: '.popup__input', // селектор инпутов
+  buttonSelector: '.popup__button-save', // селектор сабмит-кнопки
+  invalidButtonClass: 'popup__button-save_invalid', // класс для отключенной сабмит-кнопки
+  inputErrorClass: 'popup__input_type_error', // класс для инпута с ошибкой
+  errorClass: 'popup__input-error_active', // класс для самой ошибки под инпутом
+});
