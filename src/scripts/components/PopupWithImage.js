02116a3682f708b0cf23cae06b09585b766fde83
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-04-22 10:36:38 +0500

    refactor: искать элемент для функционала по элементу popup

--- src/scripts/components/PopupWithImage.js
+++ src/scripts/components/PopupWithImage.js
@@ -3,8 +3,8 @@ import Popup from './Popup.js'
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector)
-    this._image = document.querySelector('.popup__image')
-    this._name = document.querySelector('.popup__figcaption')
+    this._image = this._popup.querySelector('.popup__image')
+    this._name = this._popup.querySelector('.popup__figcaption')
   }
 
   // взять данные картинки при ее показе
